ed04e6140f10829cca36ce75d639a0b35d61dfd4
Víctor <thorviic@gmail.com>
2024-12-13 21:33:36 -0600

    Pantalla 1

---
+++ ClaroVideoJVSR/ViewModels/MovieDetailViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ClaroVideoJVSR.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ClaroVideoJVSR.ViewModels
+{
+    public class MovieDetailViewModel : INotifyPropertyChanged
+    {
+        private Movie _selectedMovie;
+        public Movie SelectedMovie
+        {
+            get => _selectedMovie;
+            set
+            {
+                _selectedMovie = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
